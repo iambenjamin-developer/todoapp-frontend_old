2084bf1eb7d0f5abd9aadc8821823f420bc08a45
Benjamin Correa <iambenjamin.developer@gmail.com>
2022-04-01 13:55:25 -0300

    To Do App

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import ToDoApp from './components/ToDoApp.vue'
+
 </script>
 
 <template>
@@ -13,7 +15,7 @@ import TheWelcome from './components/TheWelcome.vue'
   </header>
 
   <main>
-    <TheWelcome />
+    <ToDoApp />
   </main>
 </template>
 
---
+++ src/components/ToDoApp.vue
@@ -0,0 +1,53 @@
+<script setup>
+
+const toDoItem1 = {
+    id: 1,
+    name: "to Do Item 1",
+    isCompleted: true
+};
+const toDoItem2 = {
+    id: 2,
+    name: "to Do Item 2",
+    isCompleted: false
+};
+const toDoItem3 = {
+    id: 3,
+    name: "to Do Item 3",
+    isCompleted: true
+};
+
+const toDoList = [toDoItem1, toDoItem2, toDoItem3];
+
+</script>
+
+<template>
+
+    <h1>To Do List</h1>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Name</th>
+                <th scope="col">Is Completed</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">{{ toDoList[0].id }}</th>
+                <td>{{ toDoList[0].name }}</td>
+                <td>{{ toDoList[0].isCompleted }}</td>
+            </tr>
+            <tr>
+                <th scope="row">{{ toDoList[1].id }}</th>
+                <td>{{ toDoList[1].name }}</td>
+                <td>{{ toDoList[1].isCompleted }}</td>
+            </tr>
+            <tr>
+                <th scope="row">{{ toDoList[2].id }}</th>
+                <td>{{ toDoList[2].name }}</td>
+                <td>{{ toDoList[2].isCompleted }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>

e5c7b04481389619e6edcadd363480400c82370f
Benjamin Correa <iambenjamin.developer@gmail.com>
2022-04-04 07:24:23 -0300

    add v-for to table

--- src/components/ToDoApp.vue
+++ src/components/ToDoApp.vue
@@ -15,38 +15,37 @@ const toDoItem3 = {
     name: "to Do Item 3",
     isCompleted: true
 };
-
-const toDoList = [toDoItem1, toDoItem2, toDoItem3];
+const toDoItem4 = {
+    id: 4,
+    name: "to Do Item 4",
+    isCompleted: false
+};
+const toDoList = [toDoItem1, toDoItem2, toDoItem3, toDoItem4];
 
 </script>
 
 <template>
-
     <h1>To Do List</h1>
 
     <table class="table">
         <thead>
             <tr>
-                <th scope="col">Id</th>
-                <th scope="col">Name</th>
-                <th scope="col">Is Completed</th>
+                <th scope="col">
+                    <strong>Id</strong>
+                </th>
+                <th scope="col">
+                    <strong>Name</strong>
+                </th>
+                <th scope="col">
+                    <strong>Is Completed</strong>
+                </th>
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <th scope="row">{{ toDoList[0].id }}</th>
-                <td>{{ toDoList[0].name }}</td>
-                <td>{{ toDoList[0].isCompleted }}</td>
-            </tr>
-            <tr>
-                <th scope="row">{{ toDoList[1].id }}</th>
-                <td>{{ toDoList[1].name }}</td>
-                <td>{{ toDoList[1].isCompleted }}</td>
-            </tr>
-            <tr>
-                <th scope="row">{{ toDoList[2].id }}</th>
-                <td>{{ toDoList[2].name }}</td>
-                <td>{{ toDoList[2].isCompleted }}</td>
+            <tr v-for="(toDoItem, index) in toDoList" v-bind:key="index">
+                <th scope="row">{{ toDoItem.id }}</th>
+                <td>{{ toDoItem.name }}</td>
+                <td>{{ toDoItem.isCompleted }}</td>
             </tr>
         </tbody>
     </table>
